304218042f7a035c06e2ddde9a3bab562dd25f06
domingoran <91283592+domingoran@users.noreply.github.com>
2023-06-04 11:54:11 +0200

    Update solver.py

--- solver.py
+++ solver.py
@@ -8,82 +8,109 @@ import numpy as np
 import datetime
 import dimod
 import json
+import os
+import pathlib
 
-
-solutions={}
-
+def scalingCalculator(lastScaling,max,n_q):
+    return  1/(1/(lastScaling*2**(n_q))+max)
 
 n_q = 4
 t_an = 20
 num_reads = 4000
-L=10
-with open("Result10.txt", "a") as f:
-    f.write(
-        f"----------Run: {datetime.datetime.now()}---n_q = {n_q}---t_an = {t_an}---n_reads = {num_reads}---L={L}"
-    )
-    
+L=0.1
+n_sim=0
+n_scaling=2
+
 OptForce = [1313.18360656, 557.1147541 ,2191.74098361,5148.76721311,2747.82295082]
 
-# import Data of Momenta, Activations and  ArmsMatrix at time instant i
-for i in range(0,150,190):
-    ArmsMatrix = []
-    df = pd.read_csv(r"SystemSO_KE.csv")
-    Momenta = list(df.iloc[i, 5:6])
-    Arms=list(df.iloc[i, :5])
-    Activations = list(df.iloc[i, 6:-1])
-    ArmsForce = [Arms[i] * OptForce[i] for i in range(len(OptForce))]
-    ArmsMatrix.append(ArmsForce)
+for n_s in range(n_scaling+1):
+    solutions={}
+    path="Results/L"+str(L)+"/sim"+str(n_sim)
+    pathDir=path+'/nScal'+str(n_s)
+    pathlib.Path(pathDir).mkdir(parents=True, exist_ok=True) 
 
-    Obj = qe.QEncoder(
-        NumberQubits=n_q,
-        EqCoeffs=ArmsMatrix,
-        Values=[Momenta[i] -150 for i in range(len(Momenta))],
-        # Lambda=[10*abs(1 / (Momenta[0]))],
-        Lambda=[L],
-        ScaleFactors=[1,1,1,1,1], #[40,40,1.5,1,1.5],
-    )
-    Mat = Obj.CalculateMatrix()
-    solver = DWaveSampler(solver={"topology__type": "pegasus"})
+    if (os.path.exists(path+"/scaling.json") == False):
+        scaling={}
+        scaling[0]= [1.0, 1.0, 1.0, 1.0, 1.0]
+        with open(path+'/scaling.json', 'w') as f:
+            json.dump(scaling, f)
+
+    g = open(path+'/scaling.json')
+    scaling=json.load(g)
+    sf=scaling[str(n_s)]
+    for i in range(0,150,100):
+        ArmsMatrix = []
+        df = pd.read_csv(r"SystemSO_KE.csv")
+        Momenta = list(df.iloc[i, 5:6])
+        Arms=list(df.iloc[i, :5])
+        Activations = list(df.iloc[i, 6:-1])
+        ArmsForce = [Arms[i] * OptForce[i] for i in range(len(OptForce))]
+        ArmsMatrix.append(ArmsForce)
+
+        Obj = qe.QEncoder(
+            NumberQubits=n_q,
+            EqCoeffs=ArmsMatrix,
+            Values=[Momenta[i] -150 for i in range(len(Momenta))],
+            # Lambda=[10*abs(1 / (Momenta[0]))],
+            Lambda=[L],
+            ScaleFactors=sf, 
+            Coef=[]
+        )
     
+        Mat = Obj.CalculateMatrix()
+        
+        solver = DWaveSampler(solver={"topology__type": "pegasus"})
+        
 
-    bqm = dimod.BinaryQuadraticModel.from_qubo(Mat)
-    Q = (bqm.to_qubo())[0]
-    __, target_edgelist, target_adjacency = solver.structure
-    emb = find_embedding(Q, target_edgelist, verbose=1)
-    sampler = FixedEmbeddingComposite(solver, emb)
-    with open("Embedding10.txt", "a") as f:
-        f.write(f"----------Run: {datetime.datetime.now()}---n_q = {n_q} \n")
-        f.write(str(emb))
-        f.write(f"Number of physical qubits = {sum(len(chain) for chain in emb.values())} \n")
-        f.write("\n")
-    results = sampler.sample_qubo(
-        Q, num_reads=num_reads, chain_strength=10 * Mat.max(), annealing_time=t_an
-    )
-    dict_sol = results.first.sample
-    keys, values = zip(*dict_sol.items())
-    calculationTime=results.info["timing"]
+        bqm = dimod.BinaryQuadraticModel.from_qubo(Mat)
+        Q = (bqm.to_qubo())[0]
+        __, target_edgelist, target_adjacency = solver.structure
+        emb = find_embedding(Q, target_edgelist, verbose=1)
+        sampler = FixedEmbeddingComposite(solver, emb)
 
-    df1=pd.DataFrame(list(map(np.ravel, results.record["sample"][0:100])))
+        filename = pathDir+"/Result_lambda_"+str(L)+"_sim_"+str(n_sim)+"_nq_"+str(n_q)
 
-    df2 = pd.DataFrame({'Energy': results.record["energy"][0:100], 'num_occurences': results.record["num_occurrences"][0:100]})
-    df=pd.concat([df1, df2], axis="columns")
-    df.to_csv('allResults/L10/'+str(datetime.datetime.now())+"-"+str(n_q)+"-"+str(t_an)+"-"+str(L)+"-"+str(i)+'.csv')
+        results = sampler.sample_qubo(
+            Q, num_reads=num_reads, chain_strength=10 * Mat.max(), annealing_time=t_an
+        )
+        dict_sol = results.first.sample
+        keys, values = zip(*dict_sol.items())
+        
+        df1=pd.DataFrame(list(map(np.ravel, results.record["sample"][0:100])))
+        df2 = pd.DataFrame({'Energy': results.record["energy"][0:100], 'num_occurences': results.record["num_occurrences"][0:100]})
+        df=pd.concat([df1, df2], axis="columns")
+        df.to_csv(pathDir+"/Energies_lambda_"+str(L)+"_sim_"+str(n_sim)+"_nq_"+str(n_q)+'_t_'+str(i)+'.csv')
 
-    Obj.BitstringSolution = values
-    Obj.CalculateEquationValues()
-    Obj.CalculateSquareSumConstr()
+        Obj.BitstringSolution = values
+        Obj.CalculateEquationValues()
+        Obj.CalculateSquareSumConstr()
 
-    solutions[i/10]=Obj.Solution
+        solutions[i/10]=Obj.Solution
 
-    with open("Result10.txt", "a") as f:
-        f.write("\n")
-        f.write(f"Time = {i} \n")
-        f.write(f"EqValues = {Obj.EqValues} \n")
-        f.write(f"Solution = {Obj.Solution} \n")
-        f.write(f"Activations = {Activations} \n")
-        f.write(f"SquareSumConst = {Obj.SquareSumConst} \n")
-        f.write(f"CalculationTime= {calculationTime} \n")
+        n_p_q = []
+        for l in range(len(emb)):   
+            n_p_q.append(len(emb[l]))
 
-with open('solutions.json', 'w') as f:
-    json.dump(solutions, f)
+        result = ({'t': i, 
+                'constraint': Obj.EqValues, 
+                'sol': Obj.Solution, 
+                'act': Activations, 
+                'fun': Obj.SquareSumConst, 
+                'emb': n_p_q,
+                'phys_qubits': sum(len(chain) for chain in emb.values())
+                } | results.info["timing"])
 
+        pd.DataFrame([result]).to_csv(filename+".csv", mode= 'a', header = not os.path.exists(filename+".csv"))
+
+    scalingArr=[]
+    for n_act in range(5):
+        maxSol=0
+        for el in solutions.values():
+            if el[n_act]>maxSol:
+                maxSol=el[n_act]
+        scalingArr.append(scalingCalculator(sf[n_act],maxSol,n_q))
+
+            
+    scaling.update({str(n_s+1):scalingArr})
+    with open(path+'/scaling.json', 'w') as f:
+        json.dump(scaling, f)
